083f459a2fdf923b3f9b99dc866f63e0b58c912d
Kirk Clawson <kirk.clawson@varkden.com>
2015-01-08 00:17:32 -0500

    Adds HelloBoost project

---
+++ LearningCpp/HelloBoost/HelloBoost.cpp
@@ -0,0 +1,14 @@
+// HelloBoost.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <boost\array.hpp>
+#include <boost\regex.hpp>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	boost::array<int, 10> a;
+
+	return 0;
+}
+
